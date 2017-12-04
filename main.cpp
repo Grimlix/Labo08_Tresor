a7f6995645d23e69c78051c70c38d9aa54bc1ce1
grimlix <nic.hungi5@gmail.com>
2017-12-04 11:20:53 +0100

    Mise en place du rrépértoir de travail

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: nichu
+ *
+ * Created on 4. décembre 2017, 11:15
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   
+   cdcdc
+   return 0;
+}
+
